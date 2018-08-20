919e9e1ce9dd816a5c425fe3a85e7a00fdab4e24
gaorui <gillonline@qq.com>
2018-08-21 02:30:16 +0800

    fix

---
+++ client/Assets/Scripts/Services/SingletonService.cs
@@ -0,0 +1,18 @@
+class SingletonService<T> where T : new()
+{
+    static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance != null)
+                return _instance;
+            else
+            {
+                _instance = new T();
+                return _instance;
+            }
+        }
+    }
+}
